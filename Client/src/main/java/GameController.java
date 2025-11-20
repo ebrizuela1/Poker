7742e7e212800417c9e6e5b8dda89381c1363981
ebrizuela1 <106275585+ebrizuela1@users.noreply.github.com>
2025-11-20 16:14:35 -0600

    removed some reference files from homework

---
+++ Client/src/main/java/GameController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+
+public class GameController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources){}
+}
