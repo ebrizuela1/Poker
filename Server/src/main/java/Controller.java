ced64023529ae2f8ed8d649a3d2b39e6533a0681
tycrypt <100331732+tycrypt@users.noreply.github.com>
2025-11-23 18:40:01 -0600

    server gui

--- Server/src/main/java/Controller.java
+++ Server/src/main/java/Controller.java
@@ -1,2 +1,95 @@
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Controller {
+    @FXML private TextField portTextField; // Matches fx:id in ServerDisplay.fxml
+    @FXML private Button serverButton;
+    @FXML private ListView<String> logList;
+    @FXML private Label statusLabel;
+
+    private ServerThread serverThread;
+
+    public void initialize() {
+        logList.getItems().add("Server Application Started");
+    }
+
+    public void toggleServer() {
+        // Turn off server
+        if (serverThread != null && serverThread.isAlive()) {
+            serverThread.stopServer();
+            serverButton.setText("Turn ON");
+            statusLabel.setText("Status: Stopped");
+            statusLabel.setStyle("-fx-text-fill: red");
+            portTextField.setDisable(false);
+        } else { // turn on server
+            try {
+                int port = Integer.parseInt(portTextField.getText());
+                serverThread = new ServerThread(port, this);
+                serverThread.start();
+                serverButton.setText("Turn OFF");
+                statusLabel.setText("Status: Running on Port " + port);
+                statusLabel.setStyle("-fx-text-fill: green");
+                portTextField.setDisable(true);
+            } catch (NumberFormatException e) {
+                logList.getItems().add("Invalid Port Number");
+            }
+        }
+    }
+
+    // Helper to log from all the clients
+    public void updateLog(String msg) {
+        Platform.runLater(() -> {
+            logList.getItems().add(msg);
+            logList.scrollTo(logList.getItems().size() - 1);
+        });
+    }
+
+
+    public static class ServerThread extends Thread {
+        private int port;
+        private Controller callback;
+        private ServerSocket serverSocket;
+        private boolean isRunning = true;
+
+        public ServerThread(int port, Controller callback) {
+            this.port = port;
+            this.callback = callback;
+            this.setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            try (ServerSocket mysocket = new ServerSocket(port)) {
+                serverSocket = mysocket;
+                callback.updateLog("Server listening on port: " + port);
+
+                while (isRunning) {
+                    Socket connection = mysocket.accept();
+                    callback.updateLog("Client connected: " + connection.getPort());
+                    // Pass 'callback' so ClientHandler can log too
+                    ClientHandler handler = new ClientHandler(connection, callback);
+                    Thread t = new Thread(handler);
+                    t.start();
+                }
+            } catch (IOException e) {
+                if(isRunning) callback.updateLog("Server Error: " + e.getMessage());
+            }
+        }
+
+        public void stopServer() {
+            isRunning = false;
+            try {
+                if(serverSocket != null) serverSocket.close();
+            } catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
--- Server/src/main/java/MainAppServer.java
+++ Server/src/main/java/MainAppServer.java
@@ -1,85 +1,33 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MainAppServer {
-    public static void main(String[] args)throws IOException, ClassNotFoundException{
-        MainAppServer myNet = new MainAppServer();
-        myNet.serverCode();
+public class MainAppServer extends Application {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        launch(args);
     }
 
-    public void serverCode()throws IOException, ClassNotFoundException{
-        try(ServerSocket mysocket = new ServerSocket(5555)) {
-            System.out.println("Server is waiting for a client!");
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/ServerDisplay.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 700, 600);
+        stage.setScene(scene);
+        stage.setTitle("Server Dashboard");
 
-            while (true) {
-                Socket connection = mysocket.accept();
-                System.out.println("Server has a client!!!");
-                System.out.println("This is the remote port the client is using: " + connection.getPort());
-                ClientHandler handler = new ClientHandler(connection);
-                Thread t = new Thread(handler);
-                t.start();
-            }
-        }catch(IOException e){
-            System.out.println("The server has stopped working: " + e.getMessage());
-        }
-        //*/
-
-	    	/* with a serializable class
-		    	InfoPass info = (InfoPass)in.readObject();
-
-		    	System.out.println("Server received this InfoPass object: ");
-		    	System.out.println("p1: " + info.p1Choic + " p2: " + info.p2Choice);
-		    	System.out.println("p1Score: "+info.p1Score+" p2Score: "+info.p2Score);
-		    	System.out.println("password: " + info.password);
-		    	info.p1Score=12;
-		    	info.p2Score=13;
-		    	info.password = "cozmo cleared";
-		    	out.writeObject(info);
-		    	break;
-
-	    }
- 	*/
-
-	    	/*try with arraylist: works because arraylist is serializable
-	    	ArrayList<Integer> list = (ArrayList<Integer>)in.readObject();
-
-	    	System.out.println("Server received this arraylist: ");
-	    	list.forEach(i->System.out.println(i));
-	    	//out.writeObject(list.add(500));
-	    	list.add(500);
-	    	out.writeObject(list);
-	    	break;
-	    */
-
-
-        // }	//uncomment when doing networking example instead of exceptions
-
-
-    }//end of method
+        stage.setOnCloseRequest(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        stage.show();
+    }
 }
-
-//public class MainApp extends Application {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		launch(args);
-//	}
-//
-//	@Override
-//	public void start(Stage primaryStage) throws Exception {
-//        try {
-//            Parent root = FXMLLoader.load(getClass().getResource("/FXML/ServerDisplay.fxml"));
-//            Scene scene = new Scene(root, 700, 700);
-//            primaryStage.setTitle("Server Display");
-////            scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-//            primaryStage.setScene(scene);
-//            primaryStage.show();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            System.exit(1);
-//        }
-//    }
-//}
